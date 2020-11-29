353d8194138ec800a64c17ee459919c5a36730ee
silviapio <silvia.piovesan@gmail.com>
2020-11-29 19:04:59 +0100

    exercise 3 done

--- main.js
+++ main.js
@@ -68,3 +68,27 @@ function padZeros(num, totalLen) {
     const greet = who => console.log(`Hello ${who}`);
     greet("Silvia");
 }
+
+    function correctError3() {
+        var users =
+            [{ firstName: 'Homer', lastName: 'Simpson' },
+            { firstName: 'Marge', lastName: 'Simpson' },
+            { firstName: 'Bart', lastName: 'Simpson' },
+            { firstName: 'Lisa', lastName: 'Simpson' },
+            { firstName: 'Maggie', lastName: 'Simpson' }];
+        
+        let usersFirstName = users.map(user => user.firstName);
+        console.log(usersFirstName);
+    }
+
+/*
+users.map(function (user) {
+   user.firstName;
+});
+
+var users = 
+[{ firstName: 'Homer', lastName: 'Simpson' },
+{ firstName: 'Marge', lastName: 'Simpson' },
+{ firstName: 'Bart', lastName: 'Simpson' },
+{ firstName: 'Lisa', lastName: 'Simpson' },
+{ firstName: 'Maggie', lastName: 'Simpson' }];*/
