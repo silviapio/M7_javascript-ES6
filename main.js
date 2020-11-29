119d46c7a850539140ee625964a9b89900a97120
silviapio <silvia.piovesan@gmail.com>
2020-11-29 18:31:05 +0100

    exercise 2 solved

--- main.js
+++ main.js
@@ -13,3 +13,58 @@ function literals1(){
 </li>`;
     console.log(li);
 }
+
+function arrowFunctions2(){
+/*1:function multiply(num1, num2) {
+return num1 * num2;
+}*/
+    const multiply = (num1, num2) => num1*num2;
+    console.log(multiply(5, 4));
+
+/*2: function toCelsius(fahrenheit) {
+return (5/9) * (fahrenheit-32);
+}*/
+    const toCelsius = farhrenheit => (5/9)*(farhrenheit-32);
+    console.log(toCelsius(32));
+
+/*3: This function returns a string padded with leading zeros
+function padZeros(num, totalLen) {
+   var numStr = num.toString();
+   var numZeros = totalLen - numStr.length;
+   for (var i = 1; i <= numZeros; i++) {
+      numStr="0" + numStr; 
+      } 
+   return numStr;
+} */
+    const padZeros = (num, totalLen) => {
+        var numStr = num.toString();
+        var numZeros = totalLen - numStr.length;
+        for (var i = 1; i <= numZeros; i++) {
+            numStr = "0" + numStr;
+        }
+        return numStr;
+    }
+    console.log(padZeros(3,5));
+
+/*4: function power(base, exponent) { 
+   var result=1; 
+   for (var i=0; i < exponent; i++) { 
+         result *=base; 
+   } 
+   return result;
+}*/
+    const power = (base, exponent) => {
+        var result=1;
+        for (var i=0; i < exponent; i++) { 
+            result *=base; 
+        }
+        return result;
+    }
+    console.log(power(2, 4));
+
+/*5: function greet(who) {
+    console.log("Hello " + who);
+}*/
+    const greet = who => console.log(`Hello ${who}`);
+    greet("Silvia");
+}
